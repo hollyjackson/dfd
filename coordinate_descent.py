c01883675c83ef747880a46687addce6e77ee4f4
Holly Jackson <hmjackson0@gmail.com>
2026-01-06 20:17:55 -0800

    coordinate descent v2 working same as coordinate descent

--- coordinate_descent.py
+++ coordinate_descent.py
@@ -23,6 +23,270 @@ except ImportError:
 
 def mse_loss(pred, gt):
     return np.mean((gt - pred)**2)
+
+
+
+def coordinate_descent_v2(defocus_stack,  experiment_folder='experiments', gss_tol = 1e-2, gss_window = 1, ls_maxiter = 100, ls_maxiter_multiplier = None, num_epochs = 25, least_squares_first = True, save_plots = True, show_plots = False, depth_init = None, aif_init = None, dpt_denoising_weight = None, aif_denoising_weight = None, dpt_denoise_delay = 10, experiment_name = 'coord-descent', vmin = 0.7, vmax = 1.9, proxy_opt = False, beta = 1e-3, multiplier = 1.1, remove_outliers = False, diff_thresh = 2, tv_thresh = 0.15, tv_thresh_min = 0.15, tv_thresh_multiplier = None, outlier_patch_type = 'tv', adaptive_grid = False, grid_window = 0.25, gamma = 1e-3, similarity_penalty = False, finite_differences = False, t = None, fd_maxiter = 100, epsilon = 1e-3, min_Z = 0.1, max_Z = 10, num_Z = 100, k = 1, aif_method='fista', verbose=True, windowed_mse=False, test=False):
+    assert not (finite_differences and adaptive_grid)
+    assert not (finite_differences and similarity_penalty)
+    assert aif_method in ['fista', 'ls']
+
+    # important initializations ---------------------
+    
+    EXPERIMENT_NAME = experiment_name
+    experiment_folder = utils.create_experiment_folder(EXPERIMENT_NAME, base_folder=experiment_folder)
+
+    losses = []
+    
+    fs = defocus_stack.shape[0]
+    width = defocus_stack.shape[1]
+    height = defocus_stack.shape[2]
+    IMAGE_RANGE = 255. # if defocus stack in [0-255]
+    if defocus_stack.max() <= 1.5: # instead in [0-1]
+        IMAGE_RANGE = 1.
+        if verbose:
+            print('Images in range [0-1]')
+    elif verbose:
+        print('Images in range [0-255]')
+    
+    # precompute indices
+    indices = forward_model.precompute_indices(width, height)
+    sample_data = np.random.rand(indices[2].size).astype(np.float32)
+    template_A_stack = forward_model.build_fixed_pattern_csr(width, height, fs, indices[2], indices[3], sample_data)
+
+    # ------------------------------------------------
+    
+    def generate_AIF(aif, dpt, ls_maxiter, iter_folder=None):
+        # ------------------------------------------------------
+        # bounded FISTA (Nesterov's accelerated gradient method)
+        # ------------------------------------------------------
+        t0 = time.time()
+        aif = least_squares.bounded_fista_3d(dpt, defocus_stack, IMAGE_RANGE, indices=indices, template_A_stack=template_A_stack, maxiter=ls_maxiter, verbose=verbose)
+        if verbose:
+            print('FISTA duration', time.time()-t0)
+        
+        if verbose:
+            print('\nAIF result range: [',aif.min(), ',', aif.max(),']')
+        
+        loss = mse_loss(forward_model.forward(dpt, aif, indices=indices, template_A_stack=template_A_stack), defocus_stack)
+        losses.append(loss)
+        if verbose:
+            print('Loss:',loss, ', TV:',section_search.total_variation(aif))
+
+        if save_plots or show_plots:
+            plt.imshow(aif / IMAGE_RANGE)
+            if save_plots:
+                plt.savefig(os.path.join(iter_folder,'least_squares_'+str(i)+'.png'))
+            if show_plots:
+                plt.show()
+            else:
+                plt.close()
+
+        return aif, dpt
+
+    def generate_DPT(aif, dpt, beta=None, iter_folder=None, last_dpt=None):
+        # ---------------------------
+        # grid + backtracking search
+        # ---------------------------
+        
+            
+        a_b_init = None
+        t0 = time.time()
+        depth_maps, Z, k_min_indices, all_losses = section_search.grid_search_opt_k(aif, defocus_stack, indices=indices, min_Z=min_Z, max_Z=max_Z, num_Z=num_Z, k=k, beta=beta, gamma=gamma, similarity_penalty=similarity_penalty, last_dpt=last_dpt, gss_window=gss_window, verbose=verbose, windowed=windowed_mse, test=test)
+        k_min_indices = np.squeeze(k_min_indices)
+        depth_maps = np.squeeze(depth_maps)
+        if verbose:
+            print('GRID SEARCH DURATION', time.time()-t0)
+        if save_plots or show_plots:
+            plt.imshow(depth_maps)
+            plt.colorbar()
+            if save_plots:
+                plt.savefig(os.path.join(iter_folder,'grid_search_'+str(i)+'.png'))
+            if show_plots:
+                plt.show()
+            else:
+                plt.close()
+
+        
+        # GSS
+        t0 = time.time()
+        depth_map_golden = section_search.golden_section_search(Z, k_min_indices, aif, defocus_stack, indices=indices, template_A_stack=template_A_stack, window=gss_window, tolerance=gss_tol, a_b_init=a_b_init, beta=beta, gamma=gamma, similarity_penalty=similarity_penalty, last_dpt=last_dpt, verbose=verbose, windowed=False) 
+        if verbose:
+            print('GSS DURATION', time.time()-t0)
+        
+        # mse = section_search.objective_full(depth_map_golden, aif, defocus_stack, indices=indices, template_A_stack=template_A_stack, beta=beta, proxy=dpt_proxy, gamma=0, similarity_penalty=False, last_dpt=None, windowed=False)
+        # last_mse = section_search.objective_full(last_depth_map_golden, aif, defocus_stack, indices=indices, template_A_stack=template_A_stack, beta=beta, proxy=dpt_proxy, gamma=0, similarity_penalty=False, last_dpt=None, windowed=False)
+        # last_depth_map_golden = np.where(mse <= last_mse, depth_map_golden, last_depth_map_golden)
+                
+        if save_plots or show_plots:
+            plt.imshow(depth_map_golden, vmin=vmin, vmax=vmax)
+            plt.colorbar()
+            if save_plots:
+                plt.savefig(os.path.join(iter_folder,'golden_section_search_'+str(i)+'.png'))
+            if show_plots:
+                plt.show()
+            else:
+                plt.close()
+
+        dpt = depth_map_golden
+        
+        
+        loss = mse_loss(forward_model.forward(dpt, aif, indices=indices, template_A_stack=template_A_stack), defocus_stack)
+        losses.append(loss)
+
+        if verbose:
+            print('Loss:',loss,', TV:',section_search.total_variation(dpt))
+            print('\nDPT result range: [',dpt.min(), ',', dpt.max(),']')
+
+        
+        if verbose:
+            print()
+            print()
+        return aif, dpt
+
+    
+    # -------------------------------------------------------------------------------------
+    # Initialization
+    if depth_init is None:
+        # dpt = torch.full((width,height), 1).to(device, dtype=torch.float32)
+        dpt = np.ones((width, height), dtype=np.float32)
+    elif np.isscalar(depth_init):
+        # dpt = torch.full((width,height), depth_init).to(device, dtype=torch.float32)
+        dpt = np.ones((width, height), dtype=np.float32) * depth_init
+    else:
+        dpt = np.array(depth_init, dtype=np.float32)
+    
+    if not least_squares_first and aif_init is None:
+        if verbose:
+            print('initializing aif')
+        #aif = np.median(defocus_stack, axis=0) # aif as median filter of stack
+        aif = initialization.compute_aif_initialization(defocus_stack, lmbda=0.05, sharpness_measure='sobel_grad')
+    else:
+        aif = aif_init
+        
+    # -------------------------------------------------------------------------------------
+
+    
+    if show_plots:
+        if least_squares_first:
+            plt.imshow(dpt)
+            plt.title('DPT Initialization')
+            plt.show()
+        else:
+            plt.imshow(aif / IMAGE_RANGE)
+            plt.title('AIF Initialization')
+            plt.show()
+
+    
+    last_dpt = None
+    if similarity_penalty and least_squares_first:
+        # last_dpt = torch.clone(dpt.detach())
+        last_dpt = np.copy(dpt)
+
+    # coordinate descent
+    last_loss = float('inf')
+    for i in range(num_epochs):
+        
+        if verbose:
+            print('Iteration',i,'\n')
+        
+        if save_plots:
+            iter_folder = os.path.join(experiment_folder,'iteration'+str(i))
+            os.makedirs(iter_folder)
+        else:
+            iter_folder = None
+
+        if i > 0:
+            last_loss = mse_loss(forward_model.forward(dpt, aif, indices=indices, template_A_stack=template_A_stack), defocus_stack)
+
+        t0 = time.time()
+        if least_squares_first:
+            aif, dpt = generate_AIF(aif, dpt, ls_maxiter, iter_folder=iter_folder)
+            aif, dpt = generate_DPT(aif, dpt, beta=beta, iter_folder=iter_folder, last_dpt=last_dpt)
+        else:
+            aif, dpt = generate_DPT(aif, dpt, beta=beta, iter_folder=iter_folder, last_dpt=last_dpt)
+            aif, dpt = generate_AIF(aif, dpt, ls_maxiter, iter_folder=iter_folder)
+        if verbose:
+            print('FULL ITER DURATION', time.time()-t0)
+        
+        # save images themselves
+        if save_plots:
+            utils.save_dpt(iter_folder, 'dpt_'+str(i), dpt)
+            utils.save_aif(iter_folder, 'aif_'+str(i), aif / IMAGE_RANGE)
+        
+        beta *= multiplier
+        # if similarity_penalty:
+        last_dpt = np.copy(dpt)#torch.clone(dpt.detach())
+
+        if ls_maxiter_multiplier != None:
+            ls_maxiter = int(ls_maxiter * ls_maxiter_multiplier)
+            if verbose:
+                print('ls_maxiter updated to',ls_maxiter)
+            # gss_tol = gss_tol / ls_maxiter_multiplier
+            # print('gss_tol updated to', gss_tol)
+
+        # Early stopping criterion
+        loss = mse_loss(forward_model.forward(dpt, aif, indices=indices, template_A_stack=template_A_stack), defocus_stack)
+        # if loss > 1.05 * last_loss: # 5% greater
+        #     print('Loss has started to diverge, terminated early at',i,'iters')
+        #     break
+        
+        
+
+        # loss plot
+        x = np.arange(len(losses))
+        dx = int(len(losses) / (i+1))
+        
+        if save_plots:
+            plt.figure(figsize=(8, 4))
+            plt.plot(x, losses)
+            plt.scatter(x[1::dx], losses[1::dx], color='red', marker='x', s=100, label="clipped aif")
+            plt.xticks(x[::dx], labels=np.arange(len(x))[::dx]) # 6 pts per iter
+            plt.xlabel("Iteration")
+            plt.ylabel("Loss")
+            plt.title("Coordinate Descent")
+            plt.savefig(os.path.join(experiment_folder,'loss.png'))
+            plt.close()
+    
+            plt.figure(figsize=(8, 4))
+            # x = np.arange(len(losses))
+            plt.plot(x, [math.log(loss, 10) for loss in losses])
+            plt.scatter(x[1::dx], [math.log(loss, 10) for loss in losses[1::dx]], color='red', marker='x', s=100, label="clipped aif")
+            plt.xticks(x[::dx], labels=np.arange(len(x))[::dx]) # 6 pts per iter
+            plt.xlabel("Iteration")
+            plt.ylabel("log(Loss)")
+            plt.title("Coordinate Descent")
+            plt.savefig(os.path.join(experiment_folder,'log_loss.png'))
+            plt.close()
+
+        with open(os.path.join(experiment_folder,"losses.txt"), "w") as file:
+            for j in range(len(losses)):
+                if j % dx == 0:
+                    file.write("iter "+str(j // dx)+":\n")
+                item = losses[j]
+                file.write(f"{item}\n")
+    
+        if verbose:
+            print()
+            print()
+            print('--------------------------')
+            print()
+
+
+    # OPTIONAL -- remove outliers
+    if remove_outliers:
+        dpt, _ = section_search.remove_outliers(dpt, aif, diff_thresh = diff_thresh, tv_thresh = tv_thresh, patch_type = outlier_patch_type)
+        if save_plots or show_plots:
+            plt.imshow(dpt, vmin=vmin, vmax=vmax)
+            plt.colorbar()
+            if save_plots:
+                plt.savefig(os.path.join(iter_folder,'oulier_removal_'+str(i)+'.png'))
+            if show_plots:
+                plt.show()
+            else:
+                plt.close()
+        
+    return dpt, aif, ls_maxiter, experiment_folder
     
 
 def coordinate_descent(defocus_stack,  experiment_folder='experiments', gss_tol = 1e-2, gss_window = 1, ls_maxiter = 100, ls_maxiter_multiplier = None, num_epochs = 25, least_squares_first = True, save_plots = True, show_plots = False, depth_init = None, aif_init = None, dpt_denoising_weight = None, aif_denoising_weight = None, dpt_denoise_delay = 10, experiment_name = 'coord-descent', vmin = 0.7, vmax = 1.9, proxy_opt = False, beta = 1e-3, multiplier = 1.1, remove_outliers = False, diff_thresh = 2, tv_thresh = 0.15, tv_thresh_min = 0.15, tv_thresh_multiplier = None, outlier_patch_type = 'tv', adaptive_grid = False, grid_window = 0.25, gamma = 1e-3, similarity_penalty = False, finite_differences = False, t = None, fd_maxiter = 100, epsilon = 1e-3, min_Z = 0.1, max_Z = 10, num_Z = 100, k = 1, aif_method='fista', verbose=True, windowed_mse=False, test=False):
